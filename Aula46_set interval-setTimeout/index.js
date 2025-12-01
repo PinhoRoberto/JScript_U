e60485b71740351d6584c2493a5f10ca9a911778
Roberto Pinho <roberto.pinho@protonmail.com>
2025-12-01 10:41:57 -0300

    feat: add aula46 - setInterval and setTimeout

---
+++ Aula46_set interval-setTimeout/index.js
@@ -0,0 +1,10 @@
+function mostraHora() {
+    let data =  new Date();
+
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false});
+}
+function funcaoDoIntervalo() {
+    console.log (mostraHora());
+}
+setInterval(funcaoDoIntervalo, 1000);
